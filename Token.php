315c85844e2dc993b88e3eaf811ef767cba51abd
clvarley <conorlukevarley@gmail.com>
2021-01-16 23:33:23 +0000

    Added properties to the Token class

--- Token.php
+++ Token.php
@@ -10,4 +10,32 @@ namespace Swiftly\Template;
 Class Token
 {
 
+    /**
+     * Type of this token
+     *
+     * @var string $type Token type
+     */
+    public $type;
+
+    /**
+     * The line on which this token was found
+     *
+     * @var int $line_number Line number
+     */
+    public $line_number;
+
+    /**
+     * The offset on which this token was found
+     *
+     * @var int $offset Line offset
+     */
+    public $offset;
+
+    /**
+     * The content of the token
+     *
+     * @var string $content Token content
+     */
+    public $content;
+
 }
