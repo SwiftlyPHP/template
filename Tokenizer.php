d715a12e5033d07d3478cfa7281ac84d1db1b049
clvarley <conorlukevarley@gmail.com>
2021-01-26 20:34:49 +0000

    Added initial constants

--- Tokenizer.php
+++ Tokenizer.php
@@ -9,5 +9,54 @@ namespace Swiftly\Template;
  */
 Class Tokenizer
 {
-  
+
+    /**
+     * Regex for variable name/identifier
+     *
+     * @var string REGEX_IDENTIFIER
+     */
+    const REGEX_IDENTIFIER = '[A-Za-z_]';
+
+    /**
+     * Regex for start of echo tag
+     *
+     * @var string REGEX_OPEN_ECHO
+     */
+    const REGEX_OPEN_ECHO = '[[';
+
+    /**
+    * Regex for end of echo tag
+    *
+    * @var string REGEX_CLOSE
+    */
+    const REGEX_CLOSE_ECHO = ']]';
+
+    /**
+     * Regex for start of `if` statement
+     *
+     * @var string REGEX_OPEN_IF
+     */
+    const REGEX_OPEN_IF = '[%';
+
+    /**
+     * Regex for end of `if` statement
+     *
+     * @var string REGEX_CLOSE_IF
+     */
+    const REGEX_CLOSE_IF = '%]';
+
+    /**
+     * Regex for string literal
+     *
+     * @var string REGEX_LITERAL_STRING
+     */
+    const REGEX_LITERAL_STRING = '\".*\"';
+
+    /**
+     * Regex for numeric literal
+     *
+     * @var string REGEX_LITERAL_NUMERIC
+     */
+    const REGEX_LITERAL_NUMERIC = '(\d{0,}\.)?\d';
+
 }
