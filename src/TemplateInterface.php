9b30fac573ef37ecc74bfc77b5cc3dc3d5de2fd1
clvarley <conorlukevarley@gmail.com>
2021-10-14 13:25:22 +0100

    Basic idea for the render method

--- src/TemplateInterface.php
+++ src/TemplateInterface.php
@@ -2,7 +2,22 @@
 
 namespace Swiftly\Template;
 
+use Swiftly\Template\Exception\RenderException;
+
+/**
+ * Interface to represent that this class can render templates in some manner
+ */
 Interface TemplateInterface
 {
-  
+
+    /**
+     * Render the given template with the (optionally) provided data
+     *
+     * @throws RenderException   Failed to render
+     * @param string $template   Template identifier
+     * @param mixed[] $variables Template data
+     * @return string            Rendered template
+     */
+    public function render( string $template, array $variables = [] ) : string;
+
 }
