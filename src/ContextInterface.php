0956995afb15760d7bc159084af3a5433546ba5c
clvarley <conorlukevarley@gmail.com>
2021-10-14 13:31:51 +0100

    Method for creating contexts

--- src/ContextInterface.php
+++ src/ContextInterface.php
@@ -2,7 +2,20 @@
 
 namespace Swiftly\Template;
 
+/**
+ * Interface used to represent that this class can create rendering contexts
+ */
 Interface ContextInterface
 {
-  
+
+    /**
+     * Creates a new, disposable rendering context
+     *
+     * @psalm-return callable(mixed[]):string
+     *
+     * @param string $file_path Path to template
+     * @return callable         Render function
+     */
+    public function create( string $file_path ) : callable;
+
 }
