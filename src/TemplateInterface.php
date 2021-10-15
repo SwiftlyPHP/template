08a4b5e927abc6ad123d375ab4b41f2d5a80902c
clvarley <conorlukevarley@gmail.com>
2021-10-15 17:29:23 +0100

    Cant control invalid templates (handled by PHP engine)

--- src/TemplateInterface.php
+++ src/TemplateInterface.php
@@ -3,6 +3,7 @@
 namespace Swiftly\Template;
 
 use Swiftly\Template\Exception\RenderException;
+use Swiftly\Template\Exception\MissingTemplateException;
 
 /**
  * Interface to represent that this class can render templates in some manner
@@ -14,10 +15,11 @@ Interface TemplateInterface
      * Render the given template with the (optionally) provided data
      *
      * @no-named-arguments
-     * @throws RenderException   Failed to render
-     * @param string $template   Template name
-     * @param mixed[] $variables Template data
-     * @return string            Rendered template
+     * @throws RenderException          Failed to render
+     * @throws MissingTemplateException Failed to find template
+     * @param string $template          Template name
+     * @param mixed[] $variables        Template data
+     * @return string                   Rendered template
      */
     public function render( string $template, array $variables = [] ) : string;
 
