c0d17019b7a169990dfdf87660e6f30ef5b37a2e
clvarley <conorlukevarley@gmail.com>
2021-10-15 18:59:30 +0100

    Provided explanation of generic render exception

--- src/Exception/RenderException.php
+++ src/Exception/RenderException.php
@@ -4,6 +4,12 @@ namespace Swiftly\Template\Exception;
 
 use RuntimeException;
 
+/**
+ * Exception thrown when a template could not be parsed or rendered
+ *
+ * At the moment this is left as a generic stub, but in a future version it may
+ * be given a more specialised behaviour.
+ */
 Class RenderException Extends RuntimeException
 {
 
