4149e97d6bab5850a96fa5011410b0fa1d433ed6
clvarley <conorlukevarley@gmail.com>
2021-10-15 13:35:47 +0100

    Created the JSON escape class

--- src/Escape/JsonEscaper.php
+++ src/Escape/JsonEscaper.php
@@ -3,13 +3,66 @@
 namespace Swiftly\Template\Escape;
 
 use Swiftly\Template\EscapeInterface;
+use Swiftly\Template\ConfigurableInterface;
+
+use function json_encode;
+
+use const JSON_PRESERVE_ZERO_FRACTION;
 
 /**
  * Escapes content for use as JSON
  *
  * @implements EscapeInterface<mixed>
  */
-Class JsonEscaper Implements EscapeInterface
+Class JsonEscaper Implements EscapeInterface, ConfigurableInterface
 {
 
+    /** @var mixed $content */
+    private $content;
+
+    /** @var int $flags */
+    private $flags = JSON_PRESERVE_ZERO_FRACTION;
+
+    /**
+     * Prepare the given content to be converted to JSON
+     *
+     * @param mixed $content Raw content
+     */
+    public function __construct( $content )
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * Set flags to use on calls to `json_encode`
+     *
+     * @param int $flags Encoding flags
+     * @return self      Chainable
+     */
+    public function with( int $flags ) : self
+    {
+        $this->flags = $flags;
+
+        return $this;
+    }
+
+    /**
+     * Escape content as JSON
+     *
+     * @return string Escaped content
+     */
+    public function escape() : string
+    {
+        return json_encode( $this->content, $this->flags );
+    }
+
+    public function name() : string
+    {
+        return 'json';
+    }
+
+    public function __toString()
+    {
+        return $this->escape();
+    }
 }
