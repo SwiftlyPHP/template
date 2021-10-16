9ccbbbbaad1c408f0d3d30989997b15ac2dedfa3
clvarley <conorlukevarley@gmail.com>
2021-10-16 21:55:23 +0100

    Allowed throwing of the render exception again

--- src/TemplateInterface.php
+++ src/TemplateInterface.php
@@ -2,6 +2,7 @@
 
 namespace Swiftly\Template;
 
+use Swiftly\Template\Exception\RenderException;
 use Swiftly\Template\Exception\MissingTemplateException;
 
 /**
@@ -14,6 +15,7 @@ Interface TemplateInterface
      * Render the given template with the (optionally) provided data
      *
      * @no-named-arguments
+     * @throws RenderException          Failed to render
      * @throws MissingTemplateException Failed to find template
      * @param string $template          Template name
      * @param mixed[] $variables        Template data
